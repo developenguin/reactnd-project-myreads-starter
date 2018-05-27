262fe4059f72c9855065cf0154aee8638a4781b8
matthijs <matthijs@heavymetalpenguin.com>
2018-05-27 17:13:06 +0200

    Use routes for navigating between pages

--- src/BooksOverview/BooksOverview.js
+++ src/BooksOverview/BooksOverview.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class BooksOverview extends Component {
 
@@ -160,7 +161,7 @@ export default class BooksOverview extends Component {
           </div>
         </div>
         <div className="open-search">
-          <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+          <Link to="/search">Add a book</Link>
         </div>
       </div>
     )
